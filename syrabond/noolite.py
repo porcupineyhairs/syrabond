a18ca454fa3b4dbbb090ecb30e232b42b073d9b8
Ig0oo <yogson@gmail.com>
2020-03-15 13:37:15 +0300

    feature: Noolite switch introduced

--- syrabond/noolite.py
+++ syrabond/noolite.py
@@ -1,7 +1,9 @@
-import time
 import usb1
 
 from contextlib import contextmanager
+
+
+
 
 
 class NooliteBase(object):
@@ -98,20 +100,6 @@ class NooliteTX(NooliteBase):
         with self._deviceContext() as device:
             self.sendCommand(device, "brightness", channel, brightness)
 
-    def rgb(self, channel, r, g, b):
-        with self._deviceContext() as device:
-            self.sendCommand(device, "rgb", channel, r, g, b)
-
-    def executeMany(self, commands):
-        with self._deviceContext() as device:
-            for command, channel, args in commands:
-                self.sendCommand(device, command, channel, *args)
-                # Device needs a pause between commands in order to transmit the actual radio sequense
-                # Pause duration depends on device control mode (self._ctrl_mode), which has encoded bitrate and number of repetitions.
-                # When the bitrate is 2000bps, length of radio sequence is approximately 40ms(preambule) + 40ms * number of repetitions
-                # (2 << 3) + (4 << 5), Bitrate mode 2 with 4 data repetitions worked best, so 300ms delay should be more than enough.
-                time.sleep(0.3)
-
     def sendCommand(self, device, command, channel, *args):
 
         cmd = [self._ctrl_mode, 0, 0, 0, int(channel), 0, 0, 0]
@@ -141,72 +129,14 @@ class NooliteTX(NooliteBase):
         device.controlWrite(usb1.REQUEST_TYPE_CLASS | usb1.RECIPIENT_INTERFACE | usb1.ENDPOINT_OUT, 0x9, 0x300, 0, bytes(cmd), 1000)
 
 
-class NooliteRX(NooliteBase):
+class NooliteSwitch(NooliteTX):
 
-    VENDOR_ID = 5824
-    PRODUCT_ID = 1500
+    def __init__(self, channel):
+        super(NooliteTX, self).__init__()
+        self.channel = channel
 
-    def __init__(self):
-        super(NooliteBase, self).__init__()
+    def on(self):
+        self.turn_on(self.channel)
 
-        self._stopping = False
-        self._callback = lambda c, a, f, d: print("channel:     %s\naction:      %s\nfmt:    %s\ndata:        %s" % (c, a, f, d))
-
-    def _eventHandler(self, togl, input):
-        channel = input[1]
-        action = self.commandNameByIndex(input[2])
-        fmt = input[3]
-        data = list(input[4:])
-
-        self._callback(channel, action, fmt, data)
-
-    def setMessageCallback(self, callback):
-        self._callback = callback
-
-    def bindChannel(self, channel):
-        with self._deviceContext() as device:
-            self.sendCommand(device, "bind", channel)
-
-    def unbindChannel(self, channel):
-        with self._deviceContext() as device:
-            self.sendCommand(device, "unbind", channel)
-
-    def unbindAll(self):
-        with self._deviceContext() as device:
-            self.sendCommand(device, "unbind_all")
-
-    def sendCommand(self, device, command, channel, *args):
-
-        cmd = [0, int(channel), 0, 0, 0, 0, 0, 0]
-        if command == "bind":
-            cmd[0] = 1
-        elif command == "unbind":
-            cmd[0] = 3
-        elif command == "unbind_all":
-            cmd[0] = 4
-            cmd[1] = 0
-
-        device.controlWrite(usb1.REQUEST_TYPE_CLASS | usb1.RECIPIENT_INTERFACE | usb1.ENDPOINT_OUT, 0x9, 0x300, 0, bytes(cmd), 1000)
-
-    def listen(self):
-        self._stopping = False
-
-        with self._deviceContext() as device:
-            new_togl = 0;
-            prev_togl = -1;
-
-            while not self._stopping:
-                ret = device.controlRead(usb1.REQUEST_TYPE_CLASS | usb1.RECIPIENT_INTERFACE | usb1.ENDPOINT_IN, 0x9, 0x300, 0, 8, 200)
-                if len(ret) == 0:
-                    continue
-
-                new_togl = ret[0] & 63;
-
-                if new_togl != prev_togl and prev_togl != -1:
-                    self._eventHandler(new_togl, ret)
-
-                time.sleep(0.3)
-                prev_togl = new_togl
-
-    def stopListening(self):
-        self._stopping = True
+    def off(self):
+        self.turn_off(self.channel)
