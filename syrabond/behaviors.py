108fc81e3a6c68be36883bc9726c748840ef491d
Ig0oo <yogson@gmail.com>
2020-04-15 22:17:35 +0300

    feature: Behaviors!

--- syrabond/behaviors.py
+++ syrabond/behaviors.py
@@ -13,17 +13,17 @@ def invent(resource):
         if resource.timer.get('finish_time') > datetime.now():
             resource.timer.get('timer_thread', threading.Timer(1, lambda: print(1))).cancel()
             print('CANCELED!!!')
-
+            delattr(resource, 'timer')
             return
 
     if resource.state == 'ON':
-        t = threading.Timer(WORKING_TIME, resource.off)
+        t = threading.Timer(WORKING_TIME*60, resource.off)
         setattr(resource, 'timer', {
             'finish_time': datetime.now()+timedelta(minutes=WORKING_TIME),
             'timer_thread': t
         })
     if resource.state == 'OFF':
-        t = threading.Timer(STANDBY_TIME, resource.on)
+        t = threading.Timer(STANDBY_TIME*60, resource.on)
         setattr(resource, 'timer', {
             'finish_time': datetime.now()+timedelta(minutes=STANDBY_TIME),
             'timer_thread': t
